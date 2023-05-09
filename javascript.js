e69a6d72a41685cd226f1e12b470b7f913085903
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-05-10 00:37:03 +0530

    refractor : clr_scr working set

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var inp_area=document.querySelector(".input_filled");
+var res='';
+// Clear screen btn
+var clr_scr=document.querySelector(".clear_btn");
+clr_scr.addEventListener("click",function(){
+    inp_area.value="";
+});
