a31449361c3fdb7f063b4fc37694cb2499787a77
Gagandeep_Singh_101 <ts026925@gmail.com>
2023-05-10 10:39:50 +0530

    refactor set working of calculator third ,fourth and fifth row

--- javascript.js
+++ javascript.js
@@ -8,7 +8,7 @@ clr_scr.addEventListener("click",function(){
     inp_area.value="";
 });
 
-// Working with calculator first row
+// Working with calculator seocnd row
 
 // num_1 button
 var num1=document.querySelector(".num_1");
@@ -34,4 +34,80 @@ ope_add.addEventListener("click",function(){
     inp_area.value=inp_area.value+'+';
 });
 
+// Working with calculator third row
+
+// num_4 button
+var num4=document.querySelector(".num_4");
+num4.addEventListener("click",function(){
+    inp_area.value=inp_area.value+'4';
+});
+
+// num_5 button
+var num5=document.querySelector(".num_5");
+num5.addEventListener("click",function(){
+    inp_area.value=inp_area.value+'5';
+});
+
+// num_6 button
+var num6=document.querySelector(".num_6");
+num6.addEventListener("click",function(){
+    inp_area.value=inp_area.value+'6';
+});
+
+// ope_sub button
+var ope_sub=document.querySelector(".ope_sub");
+ope_sub.addEventListener("click",function(){
+    inp_area.value=inp_area.value+'+';
+});
+
+// Working with calculator fouth row
+
+// num_7 button
+var num7=document.querySelector(".num_7");
+num7.addEventListener("click",function(){
+    inp_area.value=inp_area.value+'7';
+});
+
+// num_8 button
+var num8=document.querySelector(".num_8");
+num8.addEventListener("click",function(){
+    inp_area.value=inp_area.value+'8';
+});
+
+// num_9 button
+var num9=document.querySelector(".num_9");
+num9.addEventListener("click",function(){
+    inp_area.value=inp_area.value+'9';
+});
+
+// ope_mul button
+var ope_mul=document.querySelector(".ope_mul");
+ope_mul.addEventListener("click",function(){
+    inp_area.value=inp_area.value+'*';
+});
+// Working with calculator fifth row
+
+// ope_div button
+var ope_div=document.querySelector(".ope_div");
+num7.addEventListener("click",function(){
+    inp_area.value=inp_area.value+'7';
+});
+
+// num_0 button
+var num0=document.querySelector(".num_0");
+num0.addEventListener("click",function(){
+    inp_area.value=inp_area.value+'0';
+});
+
+// dec_poi button
+var dec_poi=document.querySelector(".dec_poi");
+dec_poi.addEventListener("click",function(){
+    inp_area.value=inp_area.value+'.';
+});
+
+// ope_cal button
+var ope_cal=document.querySelector(".ope_cal");
+ope_cal.addEventListener("click",function(){
+    inp_area.value=eval(inp_area.value);
+});
 
